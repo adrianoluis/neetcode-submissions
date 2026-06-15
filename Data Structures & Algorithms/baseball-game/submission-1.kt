90d272bcd26a06a320ace488349b5cec14176828
Adriano Rocha <driflash@gmail.com>
2026-06-15 09:58:43 -0700

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.kt
@@ -0,0 +1,28 @@
+class Solution {
+    companion object {
+        const val SUM = "+"
+        const val DOUBLE = "D"
+        const val INVALIDATE = "C"
+    }
+
+    private val records = mutableListOf<Int>()
+
+    fun calPoints(operations: Array<String>): Int {
+        operations.forEach { opOrNum ->
+            val value = opOrNum.toIntOrNull()
+            when (opOrNum) {
+                SUM -> {
+                    val sum = records.takeLast(2).sum()
+                    records.add(sum)
+                }
+                DOUBLE -> {
+                    val lastDoubled = records.last() * 2
+                    records.add(lastDoubled)
+                }
+                INVALIDATE -> records.removeLast()
+                else -> value?.let(records::add)
+            }
+        }
+        return records.sum()
+    }
+}
