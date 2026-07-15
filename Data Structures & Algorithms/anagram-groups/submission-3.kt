d593df1d6b517590d2b4a4468f2485316d36a4c9
Adriano Rocha <driflash@gmail.com>
2026-07-15 12:11:14 -0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.kt
@@ -0,0 +1,31 @@
+class Solution {
+
+    fun sortString(s: String): String {
+        val freq = IntArray(26) // int array with all alphabet letters
+        s.forEach { freq[it - 'a']++ }
+        val ans = StringBuilder()
+        for (i in 0..<26) {
+            while (freq[i] > 0) {
+                ans.append((i + 'a'.code).toChar());
+                freq[i]--;
+            }
+        }
+        return ans.toString()
+    }
+
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        val dict = hashMapOf<String, Int>()
+        val list = mutableListOf<MutableList<String>>()
+        for (s in strs) {
+            val key = s.toCharArray().sorted().joinToString("")
+            if (dict.containsKey(key)) {
+                val idx = dict[key] ?: -1
+                list[idx]?.add(s)
+            } else {
+                list.add(list.size, mutableListOf(s))
+                dict[key] = list.size - 1
+            }
+        }
+        return list
+    }
+}
